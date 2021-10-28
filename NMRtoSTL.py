83dfbfc6344c6c586644beebb60039a82594b045
Matthew O'Neill <oneill_91@hotmail.co.uk>
2021-10-28 21:41:28 +0100

    Formatted with Black

--- NMRtoSTL.py
+++ NMRtoSTL.py
@@ -1,13 +1,14 @@
 from io import FileIO
 import os
+from typing import Tuple
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.tri as triang
 import numpy as np
 import re
-from process_generic import Generic
 from stl import mesh
 import nmrglue as ng
+from stl.stl import ASCII
 
 '''
 class Bruker2D:
@@ -59,20 +60,19 @@ class Bruker2D:
 '''    
     
 class importNMR:
-    
-    def __init__(self, filename:str) -> None:
+    def __init__(self, filename: str) -> None:
         self.filename = filename
-        
-    def read_file(self, verbose = False):
+
+    def read_file(self, verbose=False):
         if verbose:
             print("Reading Data")
         # read data and constants from the text file
-        try:    # read Bruker data
+        try:  # read Bruker data
             self.data, self.udic = self.readBruker(self.filename)
         except:
             raise ValueError("File format not recognised")
         # make sure that the data is 2D
-        if self.udic['ndim']!=2:
+        if self.udic["ndim"] != 2:
             raise ValueError("Data must be 2D!")
         # make ppm scales
         uc_f1 = ng.fileiobase.uc_from_udic(self.udic, dim=1)
@@ -82,7 +82,7 @@ class importNMR:
         if verbose:
             print("Plotting data")
             self.plot(uc_f1, uc_f2)
-        
+
     def readBruker(self, filename):
         # read the processed data as numpy array
         dic, data = ng.bruker.read_pdata(filename)
@@ -95,32 +95,43 @@ class importNMR:
         ppm_f1_left, ppm_f1_right = uc_f1.ppm_limits()
         ppm_f2_left, ppm_f2_right = uc_f2.ppm_limits()
         # calculate contour levels
-        cmap = matplotlib.cm.Blues_r    # contour map (colors to use for contours)
-        contour_start = 100000          # contour level start value
-        contour_num = 25                # number of contour levels
-        contour_factor = 1.20           # scaling factor between contour levels
-        cl = contour_start * contour_factor ** np.arange(contour_num) 
+        cmap = matplotlib.cm.Blues_r  # contour map (colors to use for contours)
+        contour_start = 100000  # contour level start value
+        contour_num = 25  # number of contour levels
+        contour_factor = 1.20  # scaling factor between contour levels
+        cl = contour_start * contour_factor ** np.arange(contour_num)
         # plot the data
         plt.figure()
-        plt.contour(self.data, cl, cmap=cmap, extent=(ppm_f1_left, ppm_f1_right, ppm_f2_left, ppm_f2_right))
+        plt.contour(
+            self.data,
+            cl,
+            cmap=cmap,
+            extent=(ppm_f1_left, ppm_f1_right, ppm_f2_left, ppm_f2_right),
+        )
         plt.gca().invert_yaxis()
         plt.gca().invert_xaxis()
         plt.show()
 
-    def process(self, max_height=1):
+    def process(self, max_height: int = 1) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         # expands x and y to the same s
         x, y = np.meshgrid(self.ppm_f1, self.ppm_f2)
-        z = self.data.flatten()
+        z = self.data.flatten()[::2]
         # normalise z data
-        z = (z/z.max()) * max_height
-        x = x.flatten()
-        y = y.flatten()
-        return x,y,z
-    
-def create_mesh(x:np.ndarray, y:np.ndarray, z:np.ndarray, verbose = False) -> mesh.Mesh:
+        z = (z / z.max()) * max_height
+        x = x.flatten()[::2]
+        y = y.flatten()[::2]
+        return x, y, z
+
+
+def create_mesh(
+    x: np.ndarray, y: np.ndarray, z: np.ndarray, verbose=False
+) -> mesh.Mesh:
     if verbose:
         print("Triangulating")
-    tri=triang.Triangulation(y,x,)
+    tri = triang.Triangulation(
+        y,
+        x,
+    )
     data = np.zeros(len(tri.triangles), dtype=mesh.Mesh.dtype)
     print("Constructing Mesh")
     NMR_mesh = mesh.Mesh(data, remove_empty_areas=False)
@@ -129,41 +140,24 @@ def create_mesh(x:np.ndarray, y:np.ndarray, z:np.ndarray, verbose = False) -> me
     NMR_mesh.z = z[tri.triangles]
     return NMR_mesh
 
-<<<<<<< HEAD
+
 def main(filename):
     spectrum = importNMR(filename)
-=======
-def main(filename, reader):
-    spectrum = reader(filename)
->>>>>>> 623cfb82cb9119b89d386d932e49373c7d5cc592
     spectrum.read_file(verbose=True)
-    x,y,z = spectrum.process()
-    NMR_mesh = create_mesh(x, y, z, verbose = True)
+    x, y, z = spectrum.process()
+    NMR_mesh = create_mesh(x, y, z, verbose=True)
     _, file = os.path.split(filename)
     file, _ = os.path.splitext(file)
-    filename = f'./{file}.stl'
+    filename = f"./{file}.stl"
     print(f"Saving file as: '{filename[2:]}'")
     NMR_mesh.save(filename)
-<<<<<<< HEAD
-    
-=======
-#%%
->>>>>>> 623cfb82cb9119b89d386d932e49373c7d5cc592
+
+
 if __name__ == "__main__":
     import sys
+
     try:
         filename = sys.argv[1]
     except:
-<<<<<<< HEAD
         filename = r"Example_data\Bruker_COSY\pdata\1"
     main(filename)
-=======
-        filename = "../NMR2.txt"
-    if "--generic" in sys.argv:
-        reader = Generic
-    else:
-        reader = Bruker2D
-
-    main(filename, reader=reader)
-# %%
->>>>>>> 623cfb82cb9119b89d386d932e49373c7d5cc592
