d12c8d65504a9dee4a5adbf9f2a35d8469deb3ea
Matthew O'Neill <oneill_91@hotmail.co.uk>
2021-11-02 21:15:16 +0000

    Move Import NMR to importNMR.py

--- NMRtoSTL/importNMR.py
+++ NMRtoSTL/importNMR.py
@@ -9,7 +9,6 @@ class importNMR:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-
     def read_file(self, verbose=False):
         """
         Import 2D NMR data from file.
@@ -20,7 +19,7 @@ class importNMR:
         Parameters
         ----------
         verbose : bool, optional
-            If true, additional information will be displayed. 
+            If true, additional information will be displayed.
             The default is False.
 
         Raises
@@ -35,12 +34,13 @@ class importNMR:
         """
         if verbose:
             print("Reading Data")
-            
+
         # read data and constants from the text file
-        import_functions = [self.read_Bruker_proc, 
-                            self.read_pipe
-                            #self.read_Varian
-                           ]
+        import_functions = [
+            self.read_Bruker_proc,
+            self.read_pipe
+            # self.read_Varian
+        ]
         for fun in import_functions:
             try:
                 self.data, self.udic = fun(self.filename, verbose)
@@ -49,38 +49,39 @@ class importNMR:
                 pass
         else:
             raise ValueError("File format not recognised")
-            
+
         # make sure that the data is 2D
         if self.udic["ndim"] != 2:
             raise ValueError("Data must be 2D!")
-        
+
         # make sure the the data is frequency domain
-        if self.udic[0]['freq'] == False or self.udic[1]['freq'] == False:
-            raise ValueError("Data must be frequency domain! "
-                             "Perform a Fourier transform before processing.")
-        
-        
-        
-    def make_scales(self, verbose=False, 
-                    f1_min=None, f1_max=None, f2_min=None, f2_max=None):
+        if self.udic[0]["freq"] == False or self.udic[1]["freq"] == False:
+            raise ValueError(
+                "Data must be frequency domain! "
+                "Perform a Fourier transform before processing."
+            )
+
+    def make_scales(
+        self, verbose=False, f1_min=None, f1_max=None, f2_min=None, f2_max=None
+    ):
         """
-        Creates scales for the X and Y axes in units of ppm. 
-        If F1 and F2 limits are set then the spectrum will be 
+        Creates scales for the X and Y axes in units of ppm.
+        If F1 and F2 limits are set then the spectrum will be
         cropped accordingly.
 
         Parameters
         ----------
         verbose : bool, optional
-            If true, additional information will be displayed. 
+            If true, additional information will be displayed.
             The default is False.
         f1_min : float, optional
-            Lower limit for F1 (Y) axis in ppm. 
+            Lower limit for F1 (Y) axis in ppm.
             If 'None' then the full spectral range will be used.
         f1_max : TYPE, optional
-            Upper limit for F1 (Y) axis in ppm. 
+            Upper limit for F1 (Y) axis in ppm.
             If 'None' then the full spectral range will be used.
         f2_min : TYPE, optional
-            Lower limit for F2 (X) axis in ppm. 
+            Lower limit for F2 (X) axis in ppm.
             If 'None' then the full spectral range will be used.
         f2_max : TYPE, optional
             Upper limit for F2 (X) axis in ppm.
@@ -96,7 +97,7 @@ class importNMR:
         self.ppm_f1 = uc_f1.ppm_scale()
         uc_f2 = ng.fileiobase.uc_from_udic(self.udic, dim=1)
         self.ppm_f2 = uc_f2.ppm_scale()
-        
+
         # set limits
         defaults = uc_f1.ppm_limits() + uc_f2.ppm_limits()
         limits_ppm = [f1_max, f1_min, f2_max, f2_min]
@@ -105,24 +106,23 @@ class importNMR:
             if limit == None:
                 limit = defaults[n]
                 limits_ppm[n] = limit
-        
+
         # convert limits from ppm to points
-        limits_pts.append(uc_f1.i(limits_ppm[0], 'PPM'))
-        limits_pts.append(uc_f1.i(limits_ppm[1], 'PPM'))
-        limits_pts.append(uc_f2.i(limits_ppm[2], 'PPM'))
-        limits_pts.append(uc_f2.i(limits_ppm[3], 'PPM'))
-         
+        limits_pts.append(uc_f1.i(limits_ppm[0], "PPM"))
+        limits_pts.append(uc_f1.i(limits_ppm[1], "PPM"))
+        limits_pts.append(uc_f2.i(limits_ppm[2], "PPM"))
+        limits_pts.append(uc_f2.i(limits_ppm[3], "PPM"))
+
         # apply limits
-        self.ppm_f1 = self.ppm_f1[limits_pts[0]:limits_pts[1]]
-        self.ppm_f2 = self.ppm_f2[limits_pts[2]:limits_pts[3]]
-        self.data = self.data[limits_pts[0]:limits_pts[1], 
-                              limits_pts[2]:limits_pts[3]]
-        
+        self.ppm_f1 = self.ppm_f1[limits_pts[0] : limits_pts[1]]
+        self.ppm_f2 = self.ppm_f2[limits_pts[2] : limits_pts[3]]
+        self.data = self.data[
+            limits_pts[0] : limits_pts[1], limits_pts[2] : limits_pts[3]
+        ]
+
         if verbose:
             print("Plotting data")
             self.plot(limits_ppm)
-
-
 
     def read_Bruker_proc(self, filename, verbose=False):
         """
@@ -133,7 +133,7 @@ class importNMR:
         filename : str
             File path for processed data directory '...\pdata\1'
         verbose : bool, optional
-            If true, additional information will be displayed. 
+            If true, additional information will be displayed.
             The default is False.
 
         Returns
@@ -146,18 +146,16 @@ class importNMR:
         """
         # read the processed data as numpy array
         dic, data = ng.bruker.read_pdata(filename)
-        
+
         # create a dictionary containing the experiment parameters
         udic = ng.bruker.guess_udic(dic, data, strip_fake=True)
-        
+
         # fix the time/frequency axis in the dictionary
-        udic[0]['time'], udic[1]['time'] = False, False
-        udic[0]['freq'], udic[1]['freq'] = True, True
+        udic[0]["time"], udic[1]["time"] = False, False
+        udic[0]["freq"], udic[1]["freq"] = True, True
         if verbose:
-            print('Bruker frequency data found')
+            print("Bruker frequency data found")
         return data, udic
-
-
 
     def read_pipe(self, filename, verbose=False):
         """
@@ -168,7 +166,7 @@ class importNMR:
         filename : str
             File path for NMRpipe data file '.ft2'
         verbose : bool, optional
-            If true, additional information will be displayed. 
+            If true, additional information will be displayed.
             The default is False.
 
         Returns
@@ -181,32 +179,28 @@ class importNMR:
         """
         # read the data as numpy array
         dic, data = ng.pipe.read(filename)
-        
+
         # create a dictionary containing the experiment parameters
         udic = ng.pipe.guess_udic(dic, data)
 
         if verbose:
-            print('NMRpipe data found')
+            print("NMRpipe data found")
         return data, udic
-    
-    
-    
-    '''   
+
+    """
     def read_Varian(self, filename, verbose=False):
         # read the data as numpy array
         dic, data = ng.varian.read(filename)
-        
+
         # create a dictionary containing the experiment parameters
         udic = ng.varian.guess_udic(dic, data)
-        
+
         if verbose:
             print('Varian/Agilent data found')
         return data, udic
-    '''
-    
-    
-    
-    def plot(self, limits):      
+    """
+
+    def plot(self, limits):
         """
         Plot 2D contour spectrum.
 
@@ -227,7 +221,7 @@ class importNMR:
         contour_num = 25  # number of contour levels
         contour_factor = 1.30  # scaling factor between contour levels
         cl = contour_start * contour_factor ** np.arange(contour_num)
-        
+
         # plot the data
         plt.figure()
         plt.contour(
@@ -239,8 +233,6 @@ class importNMR:
         plt.gca().invert_yaxis()
         plt.gca().invert_xaxis()
         plt.show()
-
-
 
     def process(self, max_height: int = 1) -> [np.ndarray, np.ndarray, np.ndarray]:
         # expands x and y to the same s
